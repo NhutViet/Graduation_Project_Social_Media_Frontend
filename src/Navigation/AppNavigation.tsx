be7ca1261ebbbede92e63281f8ff862230135d00
Nphuc2304 <hnthao2705@gmail.com>
2025-05-11 00:00:06 +0700

    fix

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -12,17 +12,17 @@ import {
   SeenStory,
   SeenStoryOwner,
   Setting,
-  Profile,
   SwitchAccount,
   Register,
-  Search,
   EditProfile,
   PendingMessages,
   MessageScreen,
   UserInfo,
+  MessageBox,
 } from '../Screens';
 import BottomTabs from './BottomTabs';
-import SearchResult from '../Screens/Search/Components/SearchResult';
+import SearchResult from '../(tabs)/Search/Components/SearchResult';
+import {Search} from '../(tabs)/Search';
 
 export type RootStackParamList = {
   PendingMessages: undefined;
@@ -37,7 +37,7 @@ const AppNavigator = () => {
       <Stack.Navigator
         initialRouteName="BottomTabs"
         screenOptions={{headerShown: false}}>
-          <Stack.Screen name="SearchResult" component={SearchResult} />
+        <Stack.Screen name="SearchResult" component={SearchResult} />
         <Stack.Screen name="Search" component={Search} />
         <Stack.Screen name="PostSetting" component={PostSetting} />
         <Stack.Screen name="AddPost" component={AddPost} />
@@ -49,7 +49,6 @@ const AppNavigator = () => {
         <Stack.Screen name="EditStory" component={EditStory} />
         <Stack.Screen name="EditProfile" component={EditProfile} />
         <Stack.Screen name="PendingMessages" component={PendingMessages} />
-        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen
           name="NotificationsScreen"
           component={NotificationsScreen}
@@ -60,6 +59,7 @@ const AppNavigator = () => {
         <Stack.Screen name="SeenStoryOwner" component={SeenStoryOwner} />
         <Stack.Screen name="MessageScreen" component={MessageScreen} />
         <Stack.Screen name="InfoUser" component={UserInfo} />
+        <Stack.Screen name="MessageBox" component={MessageBox} />
       </Stack.Navigator>
     </NavigationContainer>
   );
