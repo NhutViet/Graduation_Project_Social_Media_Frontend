d0fba5adde870bf0160812ff9183f76c00f54a5b
ankjrvhnoirehoei <166099409+ankjrvhnoirehoei@users.noreply.github.com>
2025-05-12 14:02:37 +0700

    fixed
    - Pending Messages screen css
    - Follower Requests screen css

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -34,7 +34,7 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="BottomTabs"
+        initialRouteName="FollowerRequests"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="PostSetting" component={PostSetting} />
         <Stack.Screen name="AddPost" component={AddPost} />
