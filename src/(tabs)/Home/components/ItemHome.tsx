bab4e1b1938a0812c281be114bbfc45863d151bf
Nphuc2304 <hnthao2705@gmail.com>
2025-05-19 00:09:31 +0700

    update date time

--- src/(tabs)/Home/components/ItemHome.tsx
+++ src/(tabs)/Home/components/ItemHome.tsx
@@ -71,6 +71,27 @@ const ItemHome = (props: any) => {
     return num?.toString();
   };
 
+  // hàm đổi ngày
+  const formatTimeAgo = (dateString: string): string => {
+    const date = new Date(dateString);
+    const now = new Date();
+    const diffMs = now.getTime() - date.getTime();
+
+    const seconds = Math.floor(diffMs / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
+
+    if (years > 0) return `${years} năm trước`;
+    if (months > 0) return `${months} tháng trước`;
+    if (days > 0) return `${days} ngày trước`;
+    if (hours > 0) return `${hours} giờ trước`;
+    if (minutes > 0) return `${minutes} phút trước`;
+    return `Vừa xong`;
+  };
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const screenWidth = Dimensions.get('window').width;
 
@@ -454,7 +475,9 @@ const ItemHome = (props: any) => {
           </TouchableOpacity>
         </View>
         <Text style={[styles.title, {color: color.text}]}>{caption}</Text>
-        <Text style={{color: color.text, fontSize: 12}}>{createdAt}</Text>
+        <Text style={{color: color.text, fontSize: 12}}>
+          {formatTimeAgo(createdAt)}
+        </Text>
       </View>
       <ModalShare
         visible={visibleModalShare}
