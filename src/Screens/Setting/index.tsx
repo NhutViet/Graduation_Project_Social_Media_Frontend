384ca7d7cd0512cb281fa3e3532161d236f08c80
crislk <quybpps40589@gmail.com>
2025-05-20 21:07:28 +0700

    Complete MusicSaving && ShowActivityStatus Screen

--- src/Screens/Setting/index.tsx
+++ src/Screens/Setting/index.tsx
@@ -20,6 +20,7 @@ import {
   Heart,
   User,
   Lock,
+  UserRoundCheck,
 } from 'lucide-react-native';
 
 export const Setting = () => {
@@ -58,6 +59,7 @@ export const Setting = () => {
                 {
                   backgroundColor: mColor.background,
                   borderBottomColor: mColor.border,
+                  borderBottomWidth: 0,
                 },
               ]}>
               <View
@@ -81,7 +83,36 @@ export const Setting = () => {
               </View>
               <ChevronRight size={20} stroke={mColor.textSecondary} />
             </TouchableOpacity>
-
+            <TouchableOpacity
+              style={[
+                styles.settingItem,
+                {
+                  backgroundColor: mColor.background,
+                  borderBottomColor: mColor.border,
+                },
+              ]}
+              onPress={() => navigation.navigate('ShowActivity')}>
+              <View
+                style={[
+                  styles.settingIconContainer,
+                  {backgroundColor: mColor.gray},
+                ]}>
+                <UserRoundCheck size={22} stroke={mColor.text} />
+              </View>
+              <View style={styles.settingContent}>
+                <Text style={[styles.settingTitle, {color: mColor.text}]}>
+                  Activity Status
+                </Text>
+                <Text
+                  style={[
+                    styles.settingDescription,
+                    {color: mColor.textSecondary},
+                  ]}>
+                  Update your activity status
+                </Text>
+              </View>
+              <ChevronRight size={20} stroke={mColor.textSecondary} />
+            </TouchableOpacity>
             {/* Tương tự cho các settingItem khác trong Account Section */}
           </View>
 
