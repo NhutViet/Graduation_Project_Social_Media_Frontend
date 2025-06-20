633743706d0544dd96b8bdd284117d5ceceba30e
ankjrvhnoirehoei <166099409+ankjrvhnoirehoei@users.noreply.github.com>
2025-06-20 21:04:56 +0700

    use

--- src/Screens/ZegoCloud/ZegoCallScreen.tsx
+++ src/Screens/ZegoCloud/ZegoCallScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Image} from 'react-native';
 import {ZegoUIKitPrebuiltCall} from '@zegocloud/zego-uikit-prebuilt-call-rn';
 import {CallAppID, CallAppSign} from '../../../services/api';
