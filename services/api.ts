e32dd52e851bb3179e31037fc8e71e82b906fa1c
Nphuc2304 <hnthao2705@gmail.com>
2025-06-20 19:08:29 +0700

    ....

--- services/api.ts
+++ services/api.ts
@@ -3,6 +3,9 @@ export const BASE_URL = 'http://cirla.io.vn';
 export const CallAppID = 1540310098;
 export const CallAppSign =
   '3c3dfbc5dcb04d97848e4a1b9e91a7bc8c66db7cbbbe41f188f40101ee33e5f6';
+export const LiveStreamAppID = 192295587;
+export const LiveStreamAppSign =
+  '0baa6cfb9bb2efb6d8c859f6ef13dba0fa6eb2df520196e63b8ae8d44bd7663e';
 
 export const API = {
   //// post
