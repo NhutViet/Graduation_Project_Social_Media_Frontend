0fbaea1d9f4937d901aa25ccb26b4c65ea0b370b
crislk <quybpps40589@gmail.com>
2025-06-22 17:41:40 +0700

    Added KeyboardAvoidingView

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import 'react-native-gesture-handler';
-import React, {useEffect} from 'react';
-import {enableScreens} from 'react-native-screens';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import React, { useEffect } from 'react';
+import { enableScreens } from 'react-native-screens';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './Navigation/AppNavigation';
-import {ThemeProvider} from './util/ThemeContext';
-import {Host} from 'react-native-portalize';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { ThemeProvider } from './util/ThemeContext';
+import { Host } from 'react-native-portalize';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'fast-text-encoding';
-import {Provider} from 'react-redux';
-import {persistor, store} from '../services/store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {UploadProvider} from '../services/UploadProgressManager';
+import { Provider } from 'react-redux';
+import { persistor, store } from '../services/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { UploadProvider } from '../services/UploadProgressManager';
 import Toast from 'react-native-toast-message';
-import {Buffer} from 'buffer';
-import {TabLoadingProvider} from '../services/TabLoadingContext';
-import {SocketProvider} from '../services/SocketContext';
-import {requestCallPermissions, setupCallKeep} from '@services/CallKeepService';
+import { Buffer } from 'buffer';
+import { TabLoadingProvider } from '../services/TabLoadingContext';
+import { SocketProvider } from '../services/SocketContext';
+import { requestCallPermissions, setupCallKeep } from '@services/CallKeepService';
+import { KeyboardAvoidingView } from 'react-native';
 global.Buffer = Buffer;
 if (__DEV__) {
   import('./config/ReactotronConfig').then(() =>
@@ -35,21 +36,25 @@ const App = () => {
   }, []);
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <SocketProvider>
             <ThemeProvider>
-              <SafeAreaProvider>
-                <Host>
-                  <UploadProvider>
-                    <TabLoadingProvider>
-                      <AppNavigator />
-                      <Toast />
-                    </TabLoadingProvider>
-                  </UploadProvider>
-                </Host>
-              </SafeAreaProvider>
+              <KeyboardAvoidingView
+                style={{ flex: 1 }}
+              >
+                <SafeAreaProvider>
+                  <Host>
+                    <UploadProvider>
+                      <TabLoadingProvider>
+                        <AppNavigator />
+                        <Toast />
+                      </TabLoadingProvider>
+                    </UploadProvider>
+                  </Host>
+                </SafeAreaProvider>
+              </KeyboardAvoidingView>
             </ThemeProvider>
           </SocketProvider>
         </PersistGate>
