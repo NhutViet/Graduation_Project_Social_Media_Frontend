8608fba09fae2da70e16bc7e20b9ce5b5cd37480
Ánh Phi <anhphi1125@gmail.com>
2025-06-30 16:37:11 +0700

    fix lỗi nhấn vào bình luận của trang người dùng

--- components/AllPostOfUserScreen.tsx
+++ components/AllPostOfUserScreen.tsx
@@ -1,12 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {useIsFocused, useNavigation, useRoute} from '@react-navigation/native';
-import {FlashList} from '@shopify/flash-list';
-import BottomSheetComment, {
-  BottomSheetCommentRef,
-} from '../src/(tabs)/Home/components/CommentSection';
-import {useDispatch, useSelector} from 'react-redux';
-import {AppDispatch, RootState} from '../services/store';
-import {fetchCommentsByPost} from '../services/commentRedux/commentSlice';
 import {
   FlatList,
   Image,
@@ -16,6 +8,12 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import {useCallback, useRef, useState} from 'react';
+import BottomSheetComment, {
+  BottomSheetCommentRef,
+} from '../src/(tabs)/Home/components/CommentSection';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '../services/store';
 import ItemHome from '../src/(tabs)/Home/components/ItemHome';
 import {useTheme} from '../src/util/ThemeContext';
 import {Colors} from '../assets/color/Colors';
@@ -47,38 +45,6 @@ const AllPostOfUserScreen = () => {
     if (id) setCurrentVisible(id);
   }, []);
 
-  const handleOpenComment = useCallback(
-    (postId: string) => {
-      console.log('Opening comment for post:', postId); // Debug log
-      setSelectedPostId(postId);
-      dispatch(fetchCommentsByPost(postId));
-      sheetRef.current?.open();
-    },
-    [dispatch],
-  );
-
-  if (!PostsItem || !Array.isArray(PostsItem)) {
-    return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={[styles.header, {backgroundColor: colors.background}]}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Image
-              source={require('../assets/icon/left.png')}
-              style={[styles.iconBack, {tintColor: colors.text}]}
-            />
-          </TouchableOpacity>
-          <Text style={[styles.title, {color: colors.text}]}>
-            Tất cả bài viết
-          </Text>
-          <View style={styles.iconBack} />
-        </View>
-        <View style={{flex: 1, backgroundColor: colors.background, justifyContent: 'center', alignItems: 'center'}}>
-          <Text style={{color: colors.text}}>No posts available</Text>
-        </View>
-      </SafeAreaView>
-    );
-  }
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={[styles.header, {backgroundColor: colors.background}]}>
@@ -93,6 +59,7 @@ const AllPostOfUserScreen = () => {
         </Text>
         <View style={styles.iconBack} />
       </View>
+
       <View style={{flex: 1, backgroundColor: colors.background}}>
         <FlatList
           ref={listRef}
@@ -105,12 +72,23 @@ const AllPostOfUserScreen = () => {
             const shouldPlay = item._id === currentVisible;
             return (
               <ItemHome
-                {...item}
-                isFocused={isFocused}
+                _id={item._id}
+                type={item.type}
+                caption={item.caption}
+                createdAt={item.createdAt}
+                media={item.media}
+                user={item.user}
+                isLike={item.isLike}
+                isBookmarked={item.isBookmarked}
+                commentCount={item.commentCount}
+                likeCount={item.likeCount}
+                share={item.share}
+                music={item.music}
                 currentVisible={shouldPlay}
-                openComment={handleOpenComment}
-                setSelectedPostId={setSelectedPostId}
+                isFocused={isFocused}
                 sheetRef={sheetRef}
+                isFollow={item.isFollow}
+                setSelectedPostId={setSelectedPostId}
               />
             );
           }}
@@ -123,8 +101,8 @@ const AllPostOfUserScreen = () => {
             minIndexForVisible: 0,
           }}
           getItemLayout={(_, index) => ({
-            length: 200,
-            offset: 200 * index,
+            length: 500,
+            offset: 500 * index,
             index,
           })}
         />
