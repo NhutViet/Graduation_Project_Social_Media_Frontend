77990ea9763ad085a7b0d5cff538185f71dc4b07
crislk <quybpps40589@gmail.com>
2025-07-05 20:19:29 +0700

    Fix: remove useless screen

--- src/Navigation/AppNavigation.tsx
+++ src/Navigation/AppNavigation.tsx
@@ -73,7 +73,6 @@ import AccountCenter from '../../src/Screens/AccountCenter';
 import InfoAccountCenter from '../../src/Screens/AccountCenter/InfoAccountCenter';
 import {User} from '@services/userRedux/userTypes';
 import PostDetailScreen from '../../components/PostDetailScreen';
-import AllTaggedPostOfUserScreen from '../../components/AllTaggedPostOfUserScreen';
 
 export type RootStackParamList = {
   MessageScreen: {
@@ -114,10 +113,6 @@ const AppNavigator = () => {
         <Stack.Screen
           name="AllPostOfUserScreen"
           component={AllPostOfUserScreen}
-        />
-        <Stack.Screen
-          name="AllTaggedPostOfUserScreen"
-          component={AllTaggedPostOfUserScreen}
         />
         <Stack.Screen name="AllReels" component={AllReels} />
         <Stack.Screen name="PostDetailScreen" component={PostDetailScreen} />
