edf18c481f072e394f739afe4b0fe0c659c6de50
Nphuc2304 <hnthao2705@gmail.com>
2025-07-13 14:03:14 +0700

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = 'http://cirla.io.vn';
-export const BASE_URL = 'http://192.168.1.5:4001';
+export const BASE_URL = 'http://cirla.io.vn';
 export const CallAppID = 41521435;
 export const CallAppSign =
   '6c6785fccbe2469324a805ea64c58069dc7254723d60c056c925ab48d2aee6a4';
