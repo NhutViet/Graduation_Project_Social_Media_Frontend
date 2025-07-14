61fb778655bd0bfa4329dfef90dd51d0f2c1ec27
Nphuc2304 <hnthao2705@gmail.com>
2025-07-14 22:35:05 +0700

    update message

--- src/Screens/Message/components/MessageInput.tsx
+++ src/Screens/Message/components/MessageInput.tsx
@@ -1,14 +1,22 @@
 import {Colors} from '@assets/color/Colors';
 import React from 'react';
-import {View, TouchableOpacity, TextInput} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import {Camera, Send, Mic, Image as ImageIcon, Plus} from 'lucide-react-native';
+import {useTheme} from '../../../../src/util/ThemeContext';
+
+const screenWidth = Dimensions.get('window').width - 20;
 
 interface MessageInputProps {
   message: string;
   setMessage: (msg: string) => void;
   sendMessage: () => void;
   pickImageAndSend: () => void;
-  styles: any;
   color: any;
 }
 
@@ -17,9 +25,11 @@ const MessageInput: React.FC<MessageInputProps> = ({
   setMessage,
   sendMessage,
   pickImageAndSend,
-  styles,
-  color,
+  // styles,
+  // color,
 }) => {
+  const {theme} = useTheme();
+  const color = Colors[theme];
   return (
     <View
       style={[
@@ -35,7 +45,7 @@ const MessageInput: React.FC<MessageInputProps> = ({
         onChangeText={setMessage}
         placeholder="Soạn tin nhắn..."
         placeholderTextColor={Colors.black}
-        style={styles.input}
+        style={[styles.input, {color: color.black}]}
         multiline={true}
         returnKeyType="default"
         blurOnSubmit={false}
@@ -47,15 +57,13 @@ const MessageInput: React.FC<MessageInputProps> = ({
         </TouchableOpacity>
       ) : (
         <View style={styles.rowContainer}>
-          <TouchableOpacity style={styles.blockIcon1}>
+          <TouchableOpacity>
             <Mic size={22} color={color.black} />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.blockIcon1}
-            onPress={pickImageAndSend}>
+          <TouchableOpacity onPress={pickImageAndSend}>
             <ImageIcon size={22} color={color.black} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.blockIcon1}>
+          <TouchableOpacity>
             <Plus size={22} color={color.black} />
           </TouchableOpacity>
         </View>
@@ -65,3 +73,35 @@ const MessageInput: React.FC<MessageInputProps> = ({
 };
 
 export default MessageInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: screenWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 30,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    margin: 10,
+    position: 'relative',
+  },
+  input: {
+    flex: 1,
+    height: 46,
+    textAlignVertical: 'center',
+  },
+  blockCamera: {
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  rowContainer: {
+    width: '25%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginRight: 5,
+  },
+});
