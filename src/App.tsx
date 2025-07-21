45f9c1b26d5e89e19541907edf264b0cfc9883cd
crislk <quybpps40589@gmail.com>
2025-07-21 13:19:11 +0700

    Fix-348: [Wrap PauseVideo context entire app]

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {enableScreens} from 'react-native-screens';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider} from './util/ThemeContext';
-import {Host} from 'react-native-portalize';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from './util/ThemeContext';
+import { Host } from 'react-native-portalize';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'fast-text-encoding';
-import {Provider} from 'react-redux';
-import {persistor, store} from '../services/store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {UploadProvider} from '../services/UploadProgressManager';
-import {Buffer} from 'buffer';
-import {TabLoadingProvider} from '../services/TabLoadingContext';
-import {SocketProvider} from '../services/SocketContext';
-import {LogBox} from 'react-native';
+import { Provider } from 'react-redux';
+import { persistor, store } from '../services/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { UploadProvider } from '../services/UploadProgressManager';
+import { Buffer } from 'buffer';
+import { TabLoadingProvider } from '../services/TabLoadingContext';
+import { SocketProvider } from '../services/SocketContext';
+import { LogBox } from 'react-native';
 import AppContent from './AppContent';
-import {HeadAlertProvider} from '../components/Global/HeadAlertProvider';
+import { HeadAlertProvider } from '../components/Global/HeadAlertProvider';
+import { VideoPauseWrapper } from './context/VideoPauseWrapper';
 
 LogBox.ignoreLogs(['Warning: componentWillReceiveProps has been renamed']);
 LogBox.ignoreLogs([
@@ -33,7 +34,7 @@ enableScreens();
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <SocketProvider>
@@ -43,7 +44,9 @@ const App = () => {
                   <UploadProvider>
                     <HeadAlertProvider>
                       <TabLoadingProvider>
-                        <AppContent />
+                        <VideoPauseWrapper>
+                          <AppContent />
+                        </VideoPauseWrapper>
                       </TabLoadingProvider>
                     </HeadAlertProvider>
                   </UploadProvider>
