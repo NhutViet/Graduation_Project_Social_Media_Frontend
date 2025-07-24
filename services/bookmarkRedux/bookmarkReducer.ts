1ef1b1c8b85d8c3fe70bcca2a0eb610dbddb878b
Ánh Phi <anhphi1125@gmail.com>
2025-07-24 17:25:11 +0700

    fix truyền dữ liệu bookmark

--- services/bookmarkRedux/bookmarkReducer.ts
+++ services/bookmarkRedux/bookmarkReducer.ts
@@ -155,11 +155,11 @@ const bookmarkReducer = createSlice({
         state.isSuccess = false;
       })
       .addCase(getItemsOfPlaylist.fulfilled, (state, action) => {
-        const {data, pagination, playlistId} = action.payload;
+        const {items, pagination, playlistId} = action.payload;
         state.isloading = false;
         state.isSuccess = true;
 
-        state.itemsByPlaylist[playlistId] = data;
+        state.itemsByPlaylist[playlistId] = items || [];
 
         state.paginationByPlaylist[playlistId] = pagination;
       })
